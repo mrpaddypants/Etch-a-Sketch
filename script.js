84484a03211de734438bee304e297799c2092cde
Patrick Wüthrich <patrick.wuethrich97@gmail.com>
2024-01-20 11:21:11 +0100

    Add style and improvements to game logic

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-const btn = document.querySelector("#button");
-btn.addEventListener("click", () => {
+const gridBtn = document.querySelector("#grid-button");
+const clearBtn = document.querySelector("#clear-button");
+
+gridBtn.addEventListener("click", () => {
     let numOfSquares = prompt("What is the desired grid-layout?");
-    if (numOfSquares < 100) {
+    if (numOfSquares <= 100) {
         numOfSquares = numOfSquares * numOfSquares;
-    createSquares(numOfSquares);
+        createSquares(numOfSquares);  
     } else {
         alert("Chosen ammount is too big. Your browser cannot handle the truth!!");
         location.reload();
     }
 });
 
+clearBtn.addEventListener("click", () => {
+    location.reload();
+})
+
+
 function createSquares(numOfSquares) {
     const container = document.getElementById("boxGrid");
-
+    const totalPixel = 640000;
+    let pxPerSquare = totalPixel / numOfSquares;
+    let sqHeightWidth = Math.sqrt(pxPerSquare);
+    
     for (i = 0; i < numOfSquares; i++) {
         const newSquare = document.createElement("div");
         newSquare.classList.add("squares");
+        newSquare.style.cssText = `height: ${sqHeightWidth}px; width: ${sqHeightWidth}px;`;
+        newSquare.addEventListener("mouseover", () => {
+            newSquare.style.backgroundColor = randomColor();
+        })
         container.appendChild(newSquare);
     }
 }
+
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
